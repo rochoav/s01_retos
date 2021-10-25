183df9eca88700ca6bc21bd30430e5da6fa34fa5
rochoav <ricardo.ochoa@telefonica.com>
2021-10-25 17:13:32 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-9tkige
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-9tkige)
---
+++ index.js
@@ -0,0 +1,6 @@
+function suma (num1,num2){
+console.log ('La suma es igual a: ');
+console.log(num1+num2);
+}
+
+suma(145,124);
